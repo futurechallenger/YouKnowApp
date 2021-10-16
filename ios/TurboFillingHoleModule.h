e12c0030afa83e13f68d9a0e9a0b7c654a6d73c2
uncle charlie <uncle_charlie@local>
2021-10-16 20:29:38 +0800

    add turbo modules in ios

---
+++ ios/TurboFillingHoleModule.h
@@ -0,0 +1,17 @@
+//
+//  TurboFillingHoleModule.h
+//  YouKnowApp
+//
+//  Created by uncle charlie on 2021/10/16.
+//
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventDispatcher.h>
+#import <React/RCTEventEmitter.h>
+#import <React/RCTBridgeModule.h>
+
+extern RCTBridge *_bridge;
+
+@interface TurboFillingHoleModule : RCTEventEmitter <RCTBridgeModule>
+
+@end
