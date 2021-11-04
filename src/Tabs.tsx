a85570c54a6dc2fe58dd7e60710ee5fcabfbced6
uncle charlie <uncle_charlie@local>
2021-11-04 13:35:57 +0800

    remove no need code

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { SettingsScreen } from './SettingsScreen';
 import { HomeNav } from './HomeNav';
@@ -8,12 +7,10 @@ const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
   return (
-    // <NavigationContainer>
     <Tab.Navigator>
       <Tab.Screen name="HomeNav" component={HomeNav} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
     </Tab.Navigator>
-    // </NavigationContainer>
   );
 };
 
